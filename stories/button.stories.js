b7d5ef2b1cbcb3686100a9ccd8b7c64095a8f1b2
Eka Prasetia <ekaone3033@gmail.com>
2020-10-30 11:52:02 +0700

    initial commit

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import { action } from '@storybook/addon-actions'
-import { Button } from '@storybook/react/demo'
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "@storybook/react/demo";
 
-export default { title: 'Button' }
+export default { title: "Tombol" };
 
 export const withText = () => (
-  <Button onClick={action('clicked')}>Hello Button</Button>
-)
+  <Button onClick={action("clicked")}>Hello Button!</Button>
+);
+
+export const withTextTwo = () => (
+  <Button onClick={action("clicked")}>Hello Text 2!</Button>
+);
+
+export const withTextThree = () => (
+  <div style={{ color: "blue", fontSize: "20px" }}>Apa aja</div>
+);
 
 export const withSomeEmoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button onClick={action("clicked")}>
     <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
+      😀 😎 👍 💯 🛂 ⛳ 🛠
     </span>
   </Button>
-)
+);
